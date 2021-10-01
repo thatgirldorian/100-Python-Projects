4b4386e6e0b367cbc74cfdeafe0f96eb6c436334
Debbie Otuagomah <debbie.otua@gmail.com>
2021-10-01 23:44:41 +0100

    Create booktitlegenerator.py

---
+++ booktitlegenerator.py
@@ -0,0 +1,11 @@
+#Create a greeting for your program.
+print("Hey there, Welcome to the fantasy name generator. 📚")
+
+#Ask the user for their favourite animal.
+fave_animal = input("\nWhat is your favourite animal?\n") 
+
+#Ask the user for their favourite season.
+season = input("\nWhat's your favourite season of the year?\n")
+
+#Combine the name of the animal & their favorite season and show them their fantasy book name.
+print(f"\nGreat! Your fantasy book title is: 'The {fave_animal} of {season}.' ")
